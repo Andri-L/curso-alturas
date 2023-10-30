40a4f8ead5df08ab22c1adb14f6e71228738e12d
Andrés <44012338+Andri-L@users.noreply.github.com>
2023-10-30 16:23:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash(__name__)
+server = app.server
 
 app.layout = html.Div([
     html.H1('Mi aplicación Dash'),
