f50ab5a90593b79ee36ca4605ec30215cde53262
Andrés <44012338+Andri-L@users.noreply.github.com>
2023-10-30 16:16:52 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1('Mi aplicación Dash'),
+    # Agrega tus componentes Dash aquí
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
